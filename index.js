7c1e17399c7457247fe6b8a200e8ecd7d34f9507
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-05-20 13:54:50 -0700

    stuck on calendar

--- index.js
+++ index.js
@@ -5,75 +5,81 @@ const axios = require("axios");
 const { Configuration, OpenAIApi } = require("openai");
 const { AuthorizationCode } = require("simple-oauth2");
 require("dotenv").config();
-
-const app = express();
-const PORT = 3000;
+const qs = require("qs");
+const { getOuterHTML } = require("domutils");
+const moment = require("moment-timezone");
 
 const THE_VERGE_URL = "https://theverge.com/";
 const WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather";
 const CITY_NAME = "Kelowna";
+const authority = "https://login.microsoftonline.com";
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const tenantId = process.env.TENANT_ID;
+const authorizationEndpoint = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`;
+const tokenEndpoint =
+  "https://login.microsoftonline.com/15519d3e-0a6e-42a1-a443-10589dd0a1de/oauth2/v2.0/token";
+const redirectURI = "http://localhost:3000/callback";
+const userId = "mj.penner@live.ca";
+const scope = "https://graph.microsoft.com/.default";
 
-// Set up the OAuth2 client
-// const client = new AuthorizationCode({
-//   client: {
-//     id: process.env.CLIENT_ID,
-//     secret: process.env.CLIENT_SECRET,
-//   },
-//   auth: {
-//     tokenHost: "https://login.microsoftonline.com",
-//     authorizePath: process.env.TENANT_ID + "/oauth2/v2.0/authorize",
-//     tokenPath: process.env.TENANT_ID + "/oauth2/v2.0/token",
-//   },
-// });
-
-// let accessToken;
-
-// app.get("/callback", async (req, res) => {
-//   const { code } = req.query;
-//   const options = {
-//     code,
-//     redirect_uri: "http://localhost:3000/callback",
-//     scope: "https://graph.microsoft.com/Calendars.Read",
-//   };
-
-//   try {
-//     accessToken = await client.getToken(options);
-//     console.log(accessToken.token.access_token);
-//     // await getOutlookEvents();
-//     await main();
-//   } catch (error) {
-//     console.error("Access Token Error", error.message);
-//   }
-//   res.send(`Access Token: ${accessToken.token.access_token}`);
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-
-async function getOutlookEvents() {
-  const today = new Date();
-  const isoDate = today.toISOString();
-  let events = [];
+async function fetchAccessToken() {
+  const requestBody = {
+    grant_type: "client_credentials",
+    client_id: clientId,
+    client_secret: clientSecret,
+    scope: scope,
+  };
 
   try {
-    const response = await axios.get(
-      `https://graph.microsoft.com/v1.0/me/calendarview?startdatetime=${isoDate}&enddatetime=${isoDate}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken.token.access_token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.post(tokenEndpoint, qs.stringify(requestBody), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
 
-    // Print the events
-    console.log(response.data.value);
-    events = response.data.value;
+    const accessToken = response.data.access_token;
+    console.log("Access token:", accessToken);
+
+    return accessToken;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching access token:", error);
   }
-  return events;
+}
+
+async function getOutlookEvents(userId, accessToken) {
+  const date = new Date();
+  date.setHours(7, 0, 0, 0);
+  const startDate = date.toISOString();
+
+  date.setDate(date.getDate());
+  date.setHours(23, 59, 0, 0);
+  const endDate = date.toISOString();
+
+  const url = `https://graph.microsoft.com/v1.0/users/${userId}/calendarView?startDateTime=${startDate}&endDateTime=${endDate}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Prefer: 'outlook.timezone="Pacific Standard Time"',
+      },
+    });
+
+    let eventString = "";
+    // console.log(response.data.value)
+    response.data.value.forEach((object) => {
+      const eventName = object.subject;
+      const startTime = object.start.dateTime;
+
+      const eventInfo = `${eventName} at ${startTime}`;
+      eventString += eventInfo + ", ";
+    });
+    console.log(eventString);
+    return eventString;
+  } catch (error) {
+    console.error("Error getting calendar events:", error);
+  }
 }
 
 async function scrapeHeadlines(url) {
@@ -116,19 +122,30 @@ async function generateGreeting(headlines, weather, events) {
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Say good morning to Myles along with an encouraging message. Tell me the forecast for the day from this file: ${weather}. Specifically,
-            tell me the high today (temp_max) as a whole number with no units, the wind speed in km/h as a whole number, 
+    prompt: `Say good morning to Myles. Tell me the forecast for the day from this file: ${weather}. Specifically,
+            tell me the maximum temperature as a whole number, the wind speed in m/s as a whole number, 
             and the current conditions (noted as 'description').
             Also tell me the top two headlines from The Verge from this list: ${headlines} as bullet points with '-' on a new paragraph.
-            Finally, summarize my daily schedule from this: ${JSON.stringify(events)}`,
-    max_tokens: 100,
+            Finally, summarize my schedule from this: ${events} on a new paragraph with bullets.
+            If an event is at 12 AM, don't specify the time.`,
+    max_tokens: 250,
     temperature: 0.4,
   });
   const ai_output = response.data.choices[0].text.trim();
   return ai_output;
 }
 
-async function sendSMS(phoneNumber, message, smtpConfig) {
+async function sendSMS(phoneNumber, message) {
+  const smtpConfig = {
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "mj.penner@live.ca",
+      pass: "zighyrfrluqiqria",
+    },
+  };
+
   const email = phoneNumber + "@txt.bell.ca";
   const transporter = nodemailer.createTransport(smtpConfig);
   const mailOptions = {
@@ -146,32 +163,23 @@ async function sendSMS(phoneNumber, message, smtpConfig) {
   }
 }
 
-async function main() {
-  const headlines = await scrapeHeadlines(THE_VERGE_URL);
-  const weather = await getForecast(process.env.OPENWEATHERMAP_API_KEY, CITY_NAME);
-  // const events = await getOutlookEvents();
-  const message = await generateGreeting(headlines, weather);
+async function sendMorningGreeting() {
+  const token = await fetchAccessToken();
+  const events = await getOutlookEvents(userId, token);
+  console.log(events);
+  //const headlines = await scrapeHeadlines(THE_VERGE_URL);
+  //const weather = await getForecast(process.env.OPENWEATHERMAP_API_KEY, CITY_NAME);
+  //const message = await generateGreeting(headlines, weather, events);
+  //console.log(message)
 
-  const smtpConfig = {
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "mj.penner@live.ca",
-      pass: "zighyrfrluqiqria",
-    },
-  };
-  sendSMS(process.env.PHONE_NUMBER, message, smtpConfig);
+  //sendSMS(process.env.PHONE_NUMBER, message);
 }
 
 process.env.TZ = "America/Los_Angeles";
 
-//generateGreeting();
-//getForecast();
-//scrapeHeadlines()
-//main()
-cron.schedule('0 7 * * *', () => {
-  main();
-});
+// cron.schedule("0 7 * * *", () => {
+//   sendMorningGreeting();
+// });
 
-//getOutlookEvents();
+sendMorningGreeting();
+//getOutlookEvents(userId)
